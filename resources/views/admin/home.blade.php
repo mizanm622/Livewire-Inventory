e66cf7b58af920518e6ef2e113c3f0b99502bc9f
Mizanur Rahman <mizanm622@gmail.com>
2023-11-23 00:10:49 +0600

    new project add

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,38 @@
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    {{ __('Admin Home') }}   <h2>Hi, {{auth()->user()->name;}} </h2>
+                </h2>
+           </x-slot>
+
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+
+                    @livewire('Counter');
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </x-app-layout>
+    </div>
+
+   <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
+
